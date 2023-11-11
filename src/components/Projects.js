c0f883d0931f375f45f9672a69126dadcc6d94f4
Divipatidar <102014528+Divipatidar@users.noreply.github.com>
2023-11-11 13:16:11 +0530

    changes

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -3,6 +3,9 @@ import './Projects.css'
 import todo from './todo.png'
 import portfolio from './portfolio.png'
 import amazone from './amazone.png'
+import brewery from './brewery.png'
+import calculator from './calculator.png'
+import survey from './survey.png'
 function Projects() {
   return (
    <section  id='project'>
@@ -37,6 +40,36 @@ function Projects() {
            <div className='item_cta'>
            <a href='https://github.com/Divipatidar/multiuser-todo' className='btn' target='_blank'>Github</a>
            <a href='https://multiuser-todo-by-divyapatidar.netlify.app' className='btn btn-primary' target='_blank'>Live Demo</a>
+           </div>
+        </article>
+        <article className='project_item'>
+           <div className='project_img'>
+              <img src={brewery} alt='img'/>
+           </div>
+           <h3>Brewery-Review-System</h3>
+           <div className='item_cta'>
+           <a href='https://github.com/Divipatidar/Brewery-review-system' className='btn' target='_blank'>Github</a>
+           <a href='https://brewery-review-system-by-divya.netlify.app' className='btn btn-primary' target='_blank'>Live Demo</a>
+           </div>
+        </article>
+        <article className='project_item'>
+           <div className='project_img'>
+              <img src={calculator} alt='img'/>
+           </div>
+           <h3>Calculator</h3>
+           <div className='item_cta'>
+           <a href='https://github.com/Divipatidar/calculator' className='btn' target='_blank'>Github</a>
+           <a href='https://calculator-by-divya.netlify.app/' className='btn btn-primary' target='_blank'>Live Demo</a>
+           </div>
+        </article>
+        <article className='project_item'>
+           <div className='project_img'>
+              <img src={survey} alt='img'/>
+           </div>
+           <h3>Survey-Form</h3>
+           <div className='item_cta'>
+           <a href='https://github.com/Divipatidar/Surveyform' className='btn' target='_blank'>Github</a>
+           <a href='https://survey-form-by-divya.netlify.app/' className='btn btn-primary' target='_blank'>Live Demo</a>
            </div>
         </article>
      </div>
