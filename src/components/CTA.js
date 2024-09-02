45dba9409aec3d350980b2ba6d245eb96a1e4b1c
Divya Patidar <divyapatidar28178@gmail.com>
2024-09-02 12:34:09 +0530

    resume update

--- src/components/CTA.js
+++ src/components/CTA.js
@@ -3,7 +3,7 @@ import React from 'react'
 function CTA() {
   return (
     <div className="cta">
-        <a href="https://drive.google.com/file/d/1W2T5n-UD96EKNiONaRDS4vf8ip9BIeTz/view?usp=sharing" target='_blank' className="btn">Download CV</a>
+        <a href="https://drive.google.com/file/d/112EWcbV-F4wxlqUL_kkeNAvTU5Xlom86/view?usp=sharing" target='_blank' className="btn">Download CV</a>
         <a href="#contact" className='btn btn-primary'>Let's Talk</a>
     </div>
   )
